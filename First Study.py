3092fc32e9074d647ce8a0cfff40def8cd85ad16
Ccat_Q <anqilacat@outlook.com>
2024-11-17 17:11:25 +0800

    询问！

--- First Study.py
+++ First Study.py
@@ -1,1 +1,7 @@
-print ("dad")
+print ("Hello,world")
+
+#询问并输出
+user_age = input ("oi!Your Age?")
+user_name = input ("oi!Your Name?")
+print ("Your age is "+user_age)
+print ("Your name is "+user_name)
