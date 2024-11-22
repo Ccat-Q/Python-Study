2e4f925e1483a7563d83410f093b65c44bd6891a
Ccat_Q <anqilacat@outlook.com>
2024-11-22 19:27:09 +0800

    len+索引

--- First Study.py
+++ First Study.py
@@ -1,7 +1,15 @@
+#运行前先保存
+#Print
+H = "Hello world"
 print ("Hello,world")
 
-#询问并输出
-user_age = input ("oi!Your Age?")
-user_name = input ("oi!Your Name?")
-print ("Your age is "+user_age)
-print ("Your name is "+user_name)
+#询问并输出,此项确认可用但Vscode无法输入，已注释
+#user_name = input ("oi!Your Name?")
+#print ("Your age is "+user_age)
+#print ("Your name is "+user_name)
+
+#使用len获取字符数量
+print (int(len(H)))
+
+#使用索引获取特定字符
+print (H[0])
